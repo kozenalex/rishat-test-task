8332f5bf09c85a2f68619925472c0ffdf18cc936
Aleksey Kosin <akosin@udm.ru>
2023-02-21 21:31:02 +0400

    added bootstrap and env-example

--- stripe_buy/management/commands/add_admin.py
+++ stripe_buy/management/commands/add_admin.py
@@ -6,8 +6,8 @@ class Command(BaseCommand):
     help = 'Adds a django admin user'
 
     def handle(self, *args, **options):
-        username = os.getenv('ADMIN_USER')
-        password = os.getenv('ADMIN_PASSWORD')
+        username = os.getenv('ADMIN_USER', 'admin')
+        password = os.getenv('ADMIN_PASSWORD', 'Qwerty123')
         user = User.objects.filter(username=username).first()
         if not user:
             User.objects.create_superuser(username=username, password=password, email='')
--- stripe_buy/settings.py
+++ stripe_buy/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'bootstrap4',
     'stripe_buy'
 ]
 
